579c8691f638f76f06e001ad9f6b47192eb8705c
manabe <manabe.mk1@gmail.com>
2021-02-05 00:45:22 +0900

    feat: mac用レイアウト

--- keymap.c
+++ keymap.c
@@ -54,8 +54,17 @@
 #define JP_AMPR KC_CIRC  // &
 #define JP_PIPE LSFT(KC_JYEN) // |
 
+/*
+ * mac
+ */
+#define KC_MCMD KC_LGUI   // command
+#define KC_MOPT KC_LALT   // option
+#define KC_MEIS KC_LANG2  // 英数
+#define KC_MKAN KC_LANG1  // かな
+
 enum layers {
     BASE,  // default layer
+    MAC,   // mac keys
     SYMB,  // symbols
     MDIA,  // media keys
 };
@@ -71,8 +80,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_DELT,           KC_NO,   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    JP_AT,
         KC_RCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_NO,             KC_BSPC, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, JP_COLN,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                                KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, JP_BSLS,
-        KC_LCTL, KC_WIN,  KC_LALT, KC_LEFT, JP_MHKN,          TG(MDIA),          TG(MDIA),         JP_HNKN, JP_LBRC, JP_RBRC, JP_YEN,  KC_RSFT,
+        KC_LCTL, KC_WIN,  KC_LALT, KC_LEFT, JP_MHKN,          TG(MAC),           TG(MDIA),         JP_HNKN, JP_LBRC, JP_RBRC, JP_YEN,  KC_RSFT,
                                              KC_SPC, KC_LCTL, KC_LALT,           KC_NO, KC_ENT, MO(SYMB)
+    ),
+
+    [MAC] = LAYOUT_moonlander(
+        _______, _______, _______, _______, _______, _______, _______,           _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______,           _______, _______, _______, _______, _______, _______, _______,
+        KC_LCTL, _______, _______, _______, _______, _______, _______,           _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______,                             _______, _______, _______, _______, _______, _______,
+        KC_MCMD, KC_MOPT, _______, _______, KC_MEIS,          _______,           _______,          KC_MKAN, _______, _______, _______, _______,
+                                            _______, _______, _______,           _______, _______, _______
     ),
 
     [SYMB] = LAYOUT_moonlander(
